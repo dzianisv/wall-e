9d16022465d2bf177820d639b475d37381955568
Den <2119348+dzianisv@users.noreply.github.com>
2024-12-16 17:09:13 +0200

    Fix tools, add rate limit retry

--- src/llm.py
+++ src/llm.py
@@ -1,7 +1,5 @@
 import logging
 from langchain_openai import ChatOpenAI
-from langchain.agents import create_structured_chat_agent, AgentExecutor, load_tools
-from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.tools import YouTubeSearchTool
 from youtube_captions_tool import YouTubeCaptionTool
 from langchain_community.utilities import OpenWeatherMapAPIWrapper
@@ -14,13 +12,15 @@ from langchain_community.tools.yahoo_finance_news import YahooFinanceNewsTool
 from langchain_community.tools import DuckDuckGoSearchResults
 
 from langchain.memory import ConversationBufferMemory
-from langchain.memory.chat_message_histories import MongoDBChatMessageHistory
+from langchain_community.chat_message_histories import MongoDBChatMessageHistory
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_core.messages import HumanMessage
 
-import threading
+import openai
 
+import threading
+import time
 
 from config import Config
 
@@ -56,22 +56,23 @@ class LLM(object):
     
     def _create_langchain(self):
         # https://langchain-ai.github.io/langgraph/how-tos/create-react-agent/#usage
-        tool_list = []
+        tools = []
 
         # https://python.langchain.com/api_reference/community/agent_toolkits/langchain_community.agent_toolkits.openapi.toolkit.RequestsToolkit.html
         toolkit = RequestsToolkit(
             requests_wrapper=TextRequestsWrapper(headers={}),
-            allow_dangerous_requests=RequestsToolkit.ALLOW_DANGEROUS_REQUEST
+            allow_dangerous_requests=True
         )
 
-        tool_list.extend(toolkit.get_tools())
+        tools.extend(toolkit.get_tools())
 
-        tool_list.extend([
+        tools.extend([
             DuckDuckGoSearchResults(), # https://python.langchain.com/docs/integrations/tools/ddg/
             YouTubeSearchTool(), # https://python.langchain.com/docs/integrations/tools/youtube/
             YouTubeCaptionTool(), 
-            OpenWeatherMapAPIWrapper(), # https://python.langchain.com/docs/integrations/tools/openweathermap/
-            StackExchangeAPIWrapper(),
+            # OpenWeatherMapAPIWrapper(), # https://python.langchain.com/docs/integrations/tools/openweathermap/
+            # TODO: throws ValueError: The first argument must be a string or a callable with a __name__ for tool decorator. Got <class 'langchain_community.utilities.stackexchange.StackExchangeAPIWrapper'>
+            # StackExchangeAPIWrapper(),  # https://python.langchain.com/api_reference/community/utilities/langchain_community.utilities.stackexchange.StackExchangeAPIWrapper.html#langchain_community.utilities.stackexchange.StackExchangeAPIWrapper
             WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper()), # https://python.langchain.com/docs/integrations/tools/wikipedia/
             YahooFinanceNewsTool(), # https://python.langchain.com/docs/integrations/tools/yahoo_finance_news/
         ])
@@ -94,10 +95,27 @@ class LLM(object):
         #     history_messages_key="agent_scratchpad",
         # )
 
+    # expect openai.RateLimitError
     def ask(self, prompt_str: str, session_id='test'):
-        response = self.chain.invoke({"messages": [HumanMessage(content=prompt_str)]}, 
-                                     {"session_id": session_id, "thread_id": threading.get_ident()}
-                                     )
+        retry_interval = 1
+        err = None
+        for _ in range(3):
+            try:
+                response = self.chain.invoke({"messages": [
+                                                    HumanMessage(content=prompt_str)
+                                            ]}, 
+                                            {"session_id": session_id, "thread_id": threading.get_ident()}
+                                            )
 
-        logger.info("Got an answer for %s: %s", session_id, response)
-        return response['messages'][-1].content
+                logger.info("Got an answer for %s: %s", session_id, response)
+                retry_interval = 1
+                return response['messages'][-1].content
+            except openai.RateLimitError as e:
+                err = e
+                logger.exception(e)
+                logger.info("Retry in %d seconds", retry_interval)
+                time.sleep(retry_interval)
+                retry_interval *= 2
+                continue
+        else:
+            raise err from None
