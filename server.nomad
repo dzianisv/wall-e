7a1b62ebfcd44f744aa770068470cb6e4f23772c
Den <2119348+dzianisv@users.noreply.github.com>
2025-02-23 21:15:19 -0800

    Use gpt-4o-mini model

--- server.nomad
+++ server.nomad
@@ -11,6 +11,7 @@ job "wall-e" {
       }
 
       env {
+        OPENAI_MODEL = "${OPENAI_MODEL}"
         OPENAI_API_KEY = "${OPENAI_API_KEY}"
         TELEGRAM_BOT_TOKEN = "${TELEGRAM_BOT_TOKEN}"
         OPENWEATHERMAP_API_KEY = "${OPENWEATHERMAP_API_KEY}"
--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ class Config:
     openai_api_base = os.environ.get("OPENAI_API_BASE")
     openai_api_key = os.environ.get("OPENAI_API_KEY")
     openai_proxy = os.environ.get("OPENAI_PROXY")
-    openai_model = os.environ.get("OPENAI_MODEL", "gpt-4o")
+    openai_model = os.environ.get("OPENAI_MODEL", "gpt-4o-mini")
     telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
